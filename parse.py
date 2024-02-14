fb0553ad14474ad1c9bae0349097fa4bd3c15756
Lukasmus <lukin.pisek@email.cz>
2024-02-14 16:52:17 +0100

    začátek

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import xml.etree.ElementTree as ET
 
 
@@ -7,7 +8,7 @@ args = sys.argv[1:]
 
 if (len(args) == 1):
     if (args[0] == "--help"):
-        print("Usage: python parse.py [options] [file]")
+        print("ahojky")
         sys.exit(0)
     else:
         sys.exit(10)
@@ -24,3 +25,54 @@ tree = ET.ElementTree(root)
 
 # Uložení stromu do souboru s definovaným kódováním
 tree.write("output.xml", encoding="UTF-8", xml_declaration=True)
+
+
+#Kontrola hlavičky
+if (sys.stdin.readline().strip() != ".IPPcode24"):
+    print("Chybná hlavička")
+    sys.exit(21)
+
+
+for line in sys.stdin:
+    #Ignorace prázdných řádků
+    if (line == os.linesep):
+        continue
+
+    #Odstranění komentářů
+    ind = line.find("#")
+    if (ind != -1):
+        line = line[:ind]
+
+    #Rozdělení řádku na slova
+    words = line.split()
+
+    #https://youtu.be/iuZIeMIlCCM?si=H7W2MCPnHikY0JpT&t=932
+    match words[0].upper():
+        case "MOVE":
+            print("MOVE")
+        case "CREATEFRAME":
+            print("CREATEFRAME")
+        case "PUSHFRAME":
+            print("PUSHFRAME")
+        case "POPFRAME":
+            print("POPFRAME")
+        case "DEFVAR":
+            print("DEFVAR")
+        case "CALL":
+            print("CALL")
+        case "RETURN":
+            print("RETURN")
+        case "PUSHS":
+            print("PUSHS")
+        case "POPS":
+            print("POPS")
+        case "ADD":
+            print("ADD")
+        case "SUB":
+            print("SUB")
+        case "MUL":
+            print("MUL")
+        case "IDIV":
+            print("IDIV")
+        
+    
